2fba96fe3f4e5e31efc149dd4ce544f4fcf0cbac
matercomus <matek.kedzia@gmail.com>
2024-06-04 20:46:03 +0200

    added move until func and impemented go straight and right in hw

--- examples/G2_Task_0/catkin_ws/src/learning_machines/src/learning_machines/test_actions.py
+++ examples/G2_Task_0/catkin_ws/src/learning_machines/src/learning_machines/test_actions.py
@@ -44,35 +44,84 @@ def test_phone_movement(rob: IRobobo):
     print("Phone tilt after move to 50: ", rob.read_phone_tilt())
 
 
+def move_until_obstacle(
+    rob: HardwareRobobo,
+    speed: int,
+    direction: str,
+    duration: int = 250,
+):
+    """Move the robot in the specified direction until an obstacle is detected
+    [BackL, BackR, FrontL, FrontR, FrontC, FrontRR, BackC, FrontLL]
+    """
+    ir_dist = 50
+    while True:
+        irs = rob.read_irs()
+        if irs[2] >= ir_dist or irs[3] >= ir_dist or irs[4] >= ir_dist:
+            print("Obstacle detected in front")
+            print(irs)
+            break
+        if irs[0] >= ir_dist or irs[1] >= ir_dist or irs[6] >= ir_dist:
+            print("Obstacle detected in the back")
+            print(irs)
+            break
+        if irs[7] >= ir_dist or irs[2] >= ir_dist:
+            print("Obstacle detected on the left")
+            print(irs)
+            break
+        if irs[3] >= ir_dist or irs[5] >= ir_dist:
+            print("Obstacle detected on the right")
+            print(irs)
+            break
+        if direction == "forward":
+            rob.move_blocking(speed, speed, duration)
+        if direction == "backward":
+            rob.move_blocking(-speed, -speed, duration)
+
+    rob.move_blocking(0, 0, 100)  # Stop the robot
+
+
+# Actions to perform in simulation
 def test_sim(rob: SimulationRobobo):
     print(rob.get_sim_time())
     print(rob.is_running())
-    rob.stop_simulation()
-    print(rob.get_sim_time())
-    print(rob.is_running())
-    rob.play_simulation()
-    print(rob.get_sim_time())
     print(rob.get_position())
+    print("IRS data: ", rob.read_irs())
+
+    ir_dist = 200
+    while (
+        rob.read_irs()[2] < ir_dist
+        and rob.read_irs()[4] < ir_dist
+        and rob.read_irs()[3] < ir_dist
+    ):
+        rob.move_blocking(25, 25, 500)
+    print("Reached obstacle")
+    # rob.move_blocking(0, 0, 100)
+    print("IRS data: ", rob.read_irs())
+    rob.move_blocking(-50, -50, 500)
+    rob.move_blocking(0, 0, 10)
+    rob.move_blocking(50, 0, 1000)
+    rob.move_blocking(50, 50, 3000)
 
 
 def test_hardware(rob: HardwareRobobo):
     print("Phone battery level: ", rob.read_phone_battery())
     print("Robot battery level: ", rob.read_robot_battery())
+    print("IRS data: ", rob.read_irs())
+
+    speed = 50
+    move_until_obstacle(rob, speed, "forward")
+    rob.move_blocking(-speed, -speed, 500)  # Move back a bit
+    rob.move_blocking(speed, -speed, 500)  # Turn right
+    rob.move_blocking(0, 0, 100)  # Stop the robot
+    move_until_obstacle(rob, speed, "forward")
 
 
 def run_all_actions(rob: IRobobo):
     if isinstance(rob, SimulationRobobo):
         rob.play_simulation()
-    test_emotions(rob)
-    test_sensors(rob)
-    test_move_and_wheel_reset(rob)
     if isinstance(rob, SimulationRobobo):
         test_sim(rob)
-
     if isinstance(rob, HardwareRobobo):
         test_hardware(rob)
-
-    test_phone_movement(rob)
-
     if isinstance(rob, SimulationRobobo):
         rob.stop_simulation()
