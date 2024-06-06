c45473bc36efa2a497604320453e25740b14edc3
matercomus <matek.kedzia@gmail.com>
2024-06-06 22:41:14 +0200

    fixed concat issue in test sim

--- examples/G2_Task_0/catkin_ws/src/learning_machines/src/learning_machines/test_actions.py
+++ examples/G2_Task_0/catkin_ws/src/learning_machines/src/learning_machines/test_actions.py
@@ -40,98 +40,6 @@ IR_SENSOR_INDICES = {
 }
 
 
-def check_obstacles(
-    rob: "HardwareRobobo", direction_map: dict, sensor_indices: dict, ir_threshold: int
-):
-    """Check for obstacles in the specified directions."""
-    irs = rob.read_irs()
-    sensors = {
-        direction: [
-            irs[sensor_indices[sensor_name]] for sensor_name in direction_map[direction]
-        ]
-        for direction in direction_map
-    }
-    obstacles = {
-        direction: any(ir >= ir_threshold for ir in sensors[direction])
-        for direction in sensors
-    }
-    return obstacles
-
-
-def turn_away_from_obstacle(
-    rob: "HardwareRobobo",
-    speed: int = 50,
-    move_duration: int = 200,
-    turn_duration: int = 100,
-    ir_threshold: int = 50,
-):
-    """Turns the robot away from detected obstacles until all front sensors are
-    clear or obstacle detected behind."""
-    speed_normal = speed
-    speed_turn = speed // 3
-
-    while True:
-        obstacles = check_obstacles(
-            rob, DIRECTION_IR_MAP, IR_SENSOR_INDICES, ir_threshold
-        )
-        obstacle_ahead = obstacles[Direction.FRONT]
-        obstacle_behind = obstacles[Direction.BACK]
-
-        if obstacle_ahead:
-            print("Obstacle detected ahead, moving backward")
-            rob.move_blocking(-speed_normal, -speed_normal, move_duration)
-            while obstacle_ahead:
-                print("Turning right")
-                rob.move_blocking(speed_turn, -speed_turn, turn_duration)
-                obstacles = check_obstacles(
-                    rob, DIRECTION_IR_MAP, IR_SENSOR_INDICES, ir_threshold
-                )
-                obstacle_ahead = obstacles[Direction.FRONT]
-                obstacle_left = obstacles[Direction.LEFT]
-                obstacle_behind = obstacles[Direction.BACK]
-
-                if not obstacle_ahead or obstacle_left or obstacle_behind:
-                    break
-        else:
-            break
-
-
-def test_hardware2(rob: "HardwareRobobo", n_seconds=120):
-    print("Phone battery level: ", rob.read_phone_battery())
-    print("Robot battery level: ", rob.read_robot_battery())
-    print("IRS data: ", rob.read_irs())
-    speed = 50
-    start_time = time.time()
-
-    while (time.time() - start_time) < n_seconds:
-        turn_away_from_obstacle(rob, speed)
-        print("Moving forward")
-        rob.move_blocking(speed, speed, 200)  # Move forward
-
-
-# Actions to perform in simulation
-def test_sim_old(rob: SimulationRobobo):
-    print(rob.get_sim_time())
-    print(rob.is_running())
-    print(rob.get_position())
-    print("IRS data: ", rob.read_irs())
-
-    ir_dist = 200
-    while (
-        rob.read_irs()[2] < ir_dist
-        and rob.read_irs()[4] < ir_dist
-        and rob.read_irs()[3] < ir_dist
-    ):
-        rob.move_blocking(25, 25, 500)
-    print("Reached obstacle")
-    # rob.move_blocking(0, 0, 100)
-    print("IRS data: ", rob.read_irs())
-    rob.move_blocking(-50, -50, 500)
-    rob.move_blocking(0, 0, 10)
-    rob.move_blocking(50, 0, 1000)
-    rob.move_blocking(50, 50, 3000)
-
-
 def write_data(data, time, irs, direction=None, event=None, run_nr=0):
     """Write sensor data to the data dictionary."""
     data["time"].append(time_now(time))
@@ -153,33 +61,51 @@ def test_hardware(rob: "HardwareRobobo", mode="HW"):
 
 def test_simulation(rob: "SimulationRobobo", mode="SIM", n_runs=11):
     # Initialize an empty DataFrame to store results
-    data = {
-        "time": [],
-        "FrontL": [],
-        "FrontR": [],
-        "FrontC": [],
-        "BackL": [],
-        "BackR": [],
-        "BackC": [],
-        "FrontLL": [],
-        "FrontRR": [],
-        "direction": [],
-        "event": [],
-        "run_nr": [],
-    }
-
-    df = pd.DataFrame(data)
+    df = pd.DataFrame(
+        columns=[
+            "time",
+            "FrontL",
+            "FrontR",
+            "FrontC",
+            "BackL",
+            "BackR",
+            "BackC",
+            "FrontLL",
+            "FrontRR",
+            "direction",
+            "event",
+            "run_nr",
+        ]
+    )
 
     for i in range(n_runs):
+        # Reset data dictionary for each run
+        data = {
+            "time": [],
+            "FrontL": [],
+            "FrontR": [],
+            "FrontC": [],
+            "BackL": [],
+            "BackR": [],
+            "BackC": [],
+            "FrontLL": [],
+            "FrontRR": [],
+            "direction": [],
+            "event": [],
+            "run_nr": [],
+        }
+
         rob.play_simulation()
         data = test(rob, run_nr=i, data=data, mode=mode, ir_threshold=200)
         rob.stop_simulation()
+
+        # Convert run data to DataFrame and concatenate
         new_df = pd.DataFrame(data)
         df = pd.concat([df, new_df], ignore_index=True)
 
     if not df.empty:
         os.makedirs("/root/results/data", exist_ok=True)
-        df.to_csv(f"/root/results/data/{mode}_run_new.csv", index=False)
+        df.to_csv(f"/root/results/data/{mode}_run_newer.csv", index=False)
 
 
 def test(
