a62b29fcb57b1c54002ee0ec548e40782a9ec907
JAY-Ronin <pablotomdio@gmail.com>
2024-06-27 20:23:50 +0200

    hardcore jebaka

--- examples/G2_Task_3/catkin_ws/src/learning_machines/src/learning_machines/test_actions.py
+++ examples/G2_Task_3/catkin_ws/src/learning_machines/src/learning_machines/test_actions.py
@@ -27,9 +27,9 @@ def run_all_actions(rob: IRobobo):
 class DQN(nn.Module):
     def __init__(self, state_size, action_size):
         super(DQN, self).__init__()
-        self.fc1 = nn.Linear(state_size, 32)
-        self.fc2 = nn.Linear(32, 32)
-        self.fc3 = nn.Linear(32, action_size)
+        self.fc1 = nn.Linear(state_size, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, action_size)
         self.initialize_weights()
 
     def initialize_weights(self):
@@ -53,13 +53,13 @@ class DQNAgent:
         self.memory = deque(maxlen=5000)
         self.gamma = 0.95  # discount rate
         self.epsilon = 1  # exploration rate
-        self.epsilon_min = 0.2
+        self.epsilon_min = 0.05
         self.epsilon_decay = 0.9
         self.learning_rate = 0.001
         self.model = DQN(state_size, action_size)
         self.criterion = nn.MSELoss()
         self.optimizer = optim.Adam(self.model.parameters(), lr=self.learning_rate)
-        self.batch_size = 64
+        self.batch_size = 128
 
     def save_model(self, file_name):
         torch.save(self.model.state_dict(), file_name)
@@ -80,7 +80,7 @@ class DQNAgent:
         return np.argmax(act_values.squeeze().cpu().numpy())
 
     def replay(self):
-        for _ in range(100):
+        for _ in range(64):
             minibatch = random.sample(self.memory, self.batch_size)
             for state, action, reward, next_state in minibatch:
                 state = torch.FloatTensor(state)
@@ -104,7 +104,7 @@ class DQNAgent:
 class train_env:
     def __init__(self, rob):
         self.rob = rob
-        self.state_size = 27  # Number of Feautures
+        self.state_size = 18  # Number of Feautures
         self.action_size = 3  # Three discrete actions: Forward, Left, Right
         self.agent = DQNAgent(self.state_size, self.action_size)
         self.csv_file = "/root/results/data.csv"
@@ -114,16 +114,13 @@ class train_env:
 
         # State values
         self.action = 0
+        self.past_actions = []
         self.ir_readings = []
-        self.position_history = []
         self.green_percent_cells = np.zeros(9)
         self.last_green_percent_cells = np.zeros(9)
         self.red_percent_cells = np.zeros(9)
         self.last_red_percent_cells = np.zeros(9)
         self.reward = 0
-        self.last_reward = 0
-        self.collected_food = 0
-        self.past_rewards = []
         self.task_flag: bool = False
 
         # Color ranges
@@ -136,25 +133,23 @@ class train_env:
 
     def values_reset(self):
         self.action = 0
+        self.past_actions = []
         self.ir_readings = []
-        self.position_history = []
         self.green_percent_cells = np.zeros(9)
         self.last_green_percent_cells = np.zeros(9)
         self.red_percent_cells = np.zeros(9)
         self.last_red_percent_cells = np.zeros(9)
         self.reward = 0
-        self.last_reward = 0
-        self.last_green_percent = 0
-        self.last_red_percent = []
-        self.collected_food = 0
-        self.past_rewards = []
         self.task_flag: bool = False
 
-    def reward_function(self):
+    def color_change(self) -> bool:
         if self.task_flag:
-            target_color = self.green_percent_cells
+            return self.green_percent_cells
         else:
-            target_color = self.red_percent_cells
+            return self.red_percent_cells
+
+    def reward_function(self):
+        target_color = self.color_change()
         # Image vertical columns
         left = (target_color[0], target_color[3], target_color[6])
         middle = (target_color[1], target_color[4], target_color[7])
@@ -172,9 +167,19 @@ class train_env:
             or self.ir_readings[2] == 1
             or self.ir_readings[3] == 1
         ):
-            reward = -10
+            return -10
+        # Penalty for turning left and right
+        l_r = [1,2,1,2]
+        r_l = [2,1,2,1]
+        l_r_2 = [2,2,1,1]
+        r_l_2 = [1,1,2,2]
+        h = self.past_actions[-4:]
+        if len(self.past_actions) >= 4 and (
+            h == l_r or h == r_l or h == l_r_2 or h == r_l_2
+            ):
+            return -3
         # Object in the middle
-        elif middle_max == 1 and left_max == 0 and right_max == 0:
+        if middle_max == 1 and left_max == 0 and right_max == 0:
             if self.action == 0:  # go forward
                 if middle.index(middle_max) == 0:
                     reward = 1
@@ -182,32 +187,31 @@ class train_env:
                     reward = 3
                 if middle.index(middle_max) == 2:
                     reward = 5
-        else:
-            # Object on the left
-            if left_max > right_max:
-                if self.action == 1:  # go left
-                    if left.index(left_max) == 0:
-                        reward = 5
-                    if left.index(left_max) == 1:
-                        reward = 3
-                    if left.index(left_max) == 2:
-                        reward = 1
-            # Object on the right
-            if left_max < right_max:
-                if self.action == 2:  # go right
-                    if right.index(right_max) == 0:
-                        reward = 5
-                    if right.index(right_max) == 1:
-                        reward = 3
-                    if right.index(right_max) == 2:
-                        reward = 1
+        # Object on the left
+        elif left_max > right_max:
+            if self.action == 1:  # go left
+                if left.index(left_max) == 0:
+                    reward = 5
+                if left.index(left_max) == 1:
+                    reward = 3
+                if left.index(left_max) == 2:
+                    reward = 1
+        # Object on the right
+        elif left_max < right_max:
+            if self.action == 2:  # go right
+                if right.index(right_max) == 0:
+                    reward = 5
+                if right.index(right_max) == 1:
+                    reward = 3
+                if right.index(right_max) == 2:
+                    reward = 1
         # Swich to objective 2 if object was close in the middle and action was forward
         if not self.task_flag and self.last_red_percent_cells[7] == 1 and max(target_color) == 0 and self.action == 0:
             self.task_flag = True
-            reward = 10
+            return 10
         # Task finished!!!
         elif self.rob.base_detects_food():
-            reward = 10
+            return 10
         return reward
 
     def step(self, state, time=200):
@@ -219,6 +223,7 @@ class train_env:
         elif self.action == 2:  # Right
             self.rob.move_blocking(15, -15, time)
 
+        self.past_actions.append(self.action)
         self.ir_readings = self.read_discrete_irs()
 
         self.last_green_percent_cells = self.green_percent_cells
@@ -226,21 +231,19 @@ class train_env:
         self.green_percent_cells, self.red_percent_cells = (
             self.get_image_green_red_percent_cells()
         )
-        self.last_reward = self.reward
         self.reward = self.reward_function()
-        self.past_rewards.append(self.reward)
 
         # action_array = np.array([self.action])
         flag_array = np.array([self.task_flag])
+        if not self.task_flag:
+            target_color = self.red_percent_cells
+        else:
+            target_color = self.green_percent_cells
         next_state = np.concatenate(
             [
-                # action_array,
                 flag_array,
                 self.ir_readings,
-                self.green_percent_cells,
-                # self.last_green_percent_cells,
-                self.red_percent_cells,
-                # self.last_red_percent_cells,
+                target_color,
             ]
         )
         self.img_id += 1
@@ -318,7 +321,7 @@ class train_env:
                 color_percent.append(1 if percent > threshold else 0)
         return color_percent
 
-    def get_image_green_red_percent_cells(self, save_img=True):
+    def get_image_green_red_percent_cells(self, save_img=False):
         save_dir = os.path.join(
             self.IMG_SAVE_DIR,
             self.run_name,
@@ -367,7 +370,9 @@ class train_env:
             return 2
         return 0
 
-    def training_loop(self):
+    def training_loop(self, load_previous_model=False):
+        if load_previous_model:
+            self.agent.load_model("/root/results/dqn_model.pth")
         print("Training started")
         for epoch in range(100):
             self.rob.stop_simulation()
@@ -378,18 +383,17 @@ class train_env:
             self.ir_readings = self.read_discrete_irs()
             # action_array = np.array([self.action])
             flag_array = np.array([self.task_flag])
+            if not self.task_flag:
+                target_color = self.red_percent_cells
+            else:
+                target_color = self.green_percent_cells
             state = np.concatenate(
                 [
-                    # action_array,
                     flag_array,
                     self.ir_readings,
-                    self.green_percent_cells,
-                    # self.last_green_percent_cells,
-                    self.red_percent_cells,
-                    # self.last_red_percent_cells,
+                    target_color,
                 ]
             )
-            print("State shape: ", state.shape)
             for _ in range(128):
                 next_state = self.step(state)
                 self.agent.remember(state, self.action, self.reward, next_state)
@@ -420,24 +424,25 @@ class train_env:
             print(f"end of {epoch + 1} epoch")
             self.agent.save_model("/root/results/dqn_model.pth")
 
-    def run_trained_model(self, max_steps=200):  # TODO update
+    def run_trained_model(self, max_steps=400):  # TODO update
         # Load the trained model
         self.rob.stop_simulation()
         self.rob.play_simulation()
         self.rob.set_phone_tilt(109, 100)
         self.agent.load_model("/root/results/dqn_model.pth")
         self.ir_readings = self.read_discrete_irs()
+        flag_array = np.array([self.task_flag])
+        if not self.task_flag:
+            target_color = self.red_percent_cells
+        else:
+            target_color = self.green_percent_cells
         state = np.concatenate(
             [
-                self.action,
+                flag_array,
                 self.ir_readings,
-                self.green_percent_cells,
-                # self.last_green_percent_cells,
-                self.red_percent_cells,
-                # self.last_red_percent_cells,
+                target_color,
             ]
         )
-        state = self.read_discrete_irs()
         total_reward = 0
         for step in range(max_steps):
             # Use the trained model to decide actions
@@ -446,8 +451,13 @@ class train_env:
             total_reward += reward
 
             # Optionally, print or log the state, action, and reward
-            print(
-                f"Step: {step}, State: {state}, Action: {self.action}, Reward: {reward}\n"
-            )
-            print(f"Total Reward: {total_reward}")
+            print("-" * 30)
+            print(f"Step: {step}")
+            print("Task:", self.task_flag)
+            print("IR readings: ", self.ir_readings)
+            print("Action: ", self.action)
+            print("Green percent cells: ", self.green_percent_cells)
+            print("Red percent cells: ", self.red_percent_cells)
+            print("Reward: ", self.reward)
+            print(f"\nTotal Reward: {total_reward}\n")
             state = next_state
